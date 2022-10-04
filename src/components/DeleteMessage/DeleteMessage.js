13ac0cc77fdd3883f361684d66c76c54d4aa1ac0
ThangHNT <thanghn2503@gmail.com>
2022-10-04 13:07:23 +0700

    update delete message modal

--- src/components/DeleteMessage/DeleteMessage.js
+++ src/components/DeleteMessage/DeleteMessage.js
@@ -1,17 +1,44 @@
+import { useContext, useState } from 'react';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './DeleteMessage.module.scss';
+import { SettingContext } from '~/components/Context/SettingContext';
 
 const cx = classNames.bind(styles);
 
 function DeleteMessage() {
+    const { handleSetDisplayRemoveMessageModal } = useContext(SettingContext);
+
+    const [selected, setSlected] = useState(false);
+
+    const handleCloseDeleteMessageModal = () => {
+        handleSetDisplayRemoveMessageModal();
+    };
+
+    const handleSelectRemoveType = (e) => {
+        const removeType = e.currentTarget.getAttribute('removetype');
+        console.log(typeof removeType);
+        setSlected(removeType);
+    };
+
     return (
         <div className={cx('wrapper')}>
+            <div className={cx('close-btn')} onClick={handleCloseDeleteMessageModal}>
+                <FontAwesomeIcon icon={faXmark} />
+            </div>
             <div className={cx('header')}>Bạn muốn gỡ tin nhắn này phía ai ?</div>
             <div className={cx('body')}>
-                <span>Thu hồi với mọi người</span>
-                <span>Xóa ở phía bạn</span>
+                <span className={cx({ chose: selected === '0' })} onClick={handleSelectRemoveType} removetype="0">
+                    Thu hồi với mọi người
+                </span>
+                <span className={cx({ chose: selected === '1' })} onClick={handleSelectRemoveType} removetype="1">
+                    Xóa ở phía bạn
+                </span>
             </div>
-            <div className={cx('footer')}>Đóng</div>
+            <div className={cx('footer')}>
+                <span className={cx('store-btn', { disabled: !selected })}>Lưu</span>
+            </div>
         </div>
     );
 }
